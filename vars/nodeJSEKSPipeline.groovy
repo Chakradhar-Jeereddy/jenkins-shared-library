3b258e8c1cbb795c993b632b6fba2c276af25ef5
chakradhar06 <chakradhar06@gmail.com>
2025-12-29 18:05:33 -0500

    u

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -1,105 +1,172 @@
-// call is the default function like main
+// call is the default function name like main method
 // Everything inside def call(){} is a function and we need to pass parameters
 // def call(map configMap), we pass map key, value pairs
 // We need to call this function from catalogue's Jenkinsfile
 // Create Jenkinsfile in catalogue repo and refer this function as "library 'my-shared-library@master'"
-def call(map configMap) {
-	pipeline{
-	 agent{
-	  node{
-	  label "agent"
-	  }
-	 }
-	 parameters {
-			string(name: 'PERSON', defaultValue: 'Mr Chakradhar', description: 'Who should I say hello to?')
-			booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-	 }
-	 environment{
-	 course = "jenkins"
-	 apiVersion = ""
-	 acc_id = "406682759639"	
-	 project = configMap.get('project')
-	 component = configMap.get('component')
-	 }
-	 options{
-	  disableConcurrentBuilds()
-	 }
-	 stages{
-		stage('Read json file'){
-		steps{
-		 script{
-		 def packagejson = readJSON file: "package.json"
-		 apiVersion = packagejson.version
-		 echo "apiversion: ${apiVersion}"
-		 }
-		}
-	  }
-	  stage('Sonar Code Analysis') {
-       when{
-        expression { false }
-       }
-	   environment{
-	   def scannerHome = tool 'sonar-8.0'
-	   }
-	   steps {
-					script {
-						withSonarQubeEnv('sonar-server') {
-						sh "${scannerHome}/bin/sonar-scanner"
-						}
-					}
-	   }
-	  }
-	  stage('Quality Gate') {
-	   steps {
-		  // Wait for Quality Gate, fail build if needed
-		  timeout(time: 1, unit: 'HOURS') {
-			waitForQualityGate abortPipeline: true
-		  }
-	   }
-	  }      
-	  stage('Build catalogue image'){
-		steps{
-		  withAWS(region:'us-east-1',credentials:'aws-ecr') {
-		  sh"""
-		  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${acc_id}.dkr.ecr.us-east-1.amazonaws.com
-		  docker build -t ${acc_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${apiVersion} .
-		  docker push ${acc_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${apiVersion}
-		  """
-		  }
-		}
-	  }
-	  stage('Test'){
-		environment{
-		name = "chakras testing"
-		}
-		steps{
-		echo "Testing"
-		echo "${name}"
-		}
-	  }
-	  stage('Deploy'){
-		when{
-		expression { params.Deploy == "true" }
-		}
-		steps{
-		echo "Deploying"
-		}
-	  }
-	 }
-	 post{
-	  always{
-	   cleanWs()
-	   echo "Always say hi"
-	  }
-	  success{
-	  echo "Passed the deployment"
-	  }
-	  failure{
-	  echo "Deployment failed"
-	  }
-	  aborted{
-	  echo "deployment aborted"
-	  }
-	 }
-	} 
+
+def call (Map configMap){
+    pipeline {
+    // These are pre-build sections
+        agent {
+            node {
+                label 'agent'
+            }
+        }
+        environment {
+            COURSE = "Jenkins"
+            appVersion = ""
+            ACC_ID = "406682759639"
+            PROJECT = configMap.get("project")
+            COMPONENT = configMap.get("component")
+        }
+        options {
+            timeout(time: 10, unit: 'MINUTES') 
+            disableConcurrentBuilds()
+        }
+        // This is build section
+        stages {
+            stage('Read Version') {
+                steps {
+                    script{
+                        def packageJSON = readJSON file: 'package.json'
+                        appVersion = packageJSON.version
+                        echo "app version: ${appVersion}"
+                    }
+                }
+            }
+            stage('Unit Test') {
+                steps {
+                    script{
+                        sh """
+                            npm test
+                        """
+                    }
+                }
+            }
+            //Here you need to select scanner tool and send the analysis to server
+            /* stage('Sonar Scan'){
+                environment {
+                    def scannerHome = tool 'sonar-8.0'
+                }
+                steps {
+                    script{
+                        withSonarQubeEnv('sonar-server') {
+                            sh  "${scannerHome}/bin/sonar-scanner"
+                        }
+                    }
+                }
+            }
+            stage('Quality Gate') {
+                steps {
+                    timeout(time: 1, unit: 'HOURS') {
+                        // Wait for the quality gate status
+                        // abortPipeline: true will fail the Jenkins job if the quality gate is 'FAILED'
+                        waitForQualityGate abortPipeline: true 
+                    }
+                }
+            } */
+            stage('Dependabot Security Gate') {
+                when {
+                    expression { false }
+                }
+                environment {
+                    GITHUB_OWNER = 'daws-86s'
+                    GITHUB_REPO  = 'catalogue'
+                    GITHUB_API   = 'https://api.github.com'
+                    GITHUB_TOKEN = credentials('GITHUB_TOKEN')
+                }
+
+                steps {
+                    script{
+                        /* Use sh """ when you want to use Groovy variables inside the shell.
+                        Use sh ''' when you want the script to be treated as pure shell. */
+                        sh '''
+                        echo "Fetching Dependabot alerts..."
+
+                        response=$(curl -s \
+                            -H "Authorization: token ${GITHUB_TOKEN}" \
+                            -H "Accept: application/vnd.github+json" \
+                            "${GITHUB_API}/repos/${GITHUB_OWNER}/${GITHUB_REPO}/dependabot/alerts?per_page=100")
+
+                        echo "${response}" > dependabot_alerts.json
+
+                        high_critical_open_count=$(echo "${response}" | jq '[.[] 
+                            | select(
+                                .state == "open"
+                                and (.security_advisory.severity == "high"
+                                    or .security_advisory.severity == "critical")
+                            )
+                        ] | length')
+
+                        echo "Open HIGH/CRITICAL Dependabot alerts: ${high_critical_open_count}"
+
+                        if [ "${high_critical_open_count}" -gt 0 ]; then
+                            echo "❌ Blocking pipeline due to OPEN HIGH/CRITICAL Dependabot alerts"
+                            echo "Affected dependencies:"
+                            echo "$response" | jq '.[] 
+                            | select(.state=="open" 
+                            and (.security_advisory.severity=="high" 
+                            or .security_advisory.severity=="critical"))
+                            | {dependency: .dependency.package.name, severity: .security_advisory.severity, advisory: .security_advisory.summary}'
+                            exit 1
+                        else
+                            echo "✅ No OPEN HIGH/CRITICAL Dependabot alerts found"
+                        fi
+                        '''
+                        
+                    }
+                }
+            }
+
+            stage('Build Image') {
+                steps {
+                    script{
+                        withAWS(region:'us-east-1',credentials:'aws-ecr') {
+                            sh """
+                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker images
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                        }
+                    }
+                }
+            }
+            
+            /* stage('Trivy Scan'){
+                steps {
+                    script{
+                        sh """
+                            trivy image \
+                            --scanners vuln \
+                            --severity HIGH,CRITICAL,MEDIUM \
+                            --pkg-types os \
+                            --exit-code 1 \
+                            --format table \
+                            ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                        """
+                    }
+                }
+            } */
+
+        }
+
+            
+
+        post{
+            always{
+                echo 'I will always say Hello again!'
+                cleanWs()
+            }
+            success {
+                echo 'I will run if success'
+            }
+            failure {
+                echo 'I will run if failure'
+            }
+            aborted {
+                echo 'pipeline is aborted'
+            }
+        }
+    }
 }
